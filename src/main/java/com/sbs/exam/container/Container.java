d5273cce709a880425b722bbe7c461cebb61c282
jaehwaJo <woghk2233@naver.com>
2023-04-27 21:00:43 +0900

    BeforeActionInterceptor 추가, homeController 도입

--- src/main/java/com/sbs/exam/container/Container.java
+++ src/main/java/com/sbs/exam/container/Container.java
@@ -1,7 +1,11 @@
 package com.sbs.exam.container;
 
 import com.sbs.exam.controller.ArticleController;
+import com.sbs.exam.controller.HomeController;
 import com.sbs.exam.controller.MemberController;
+import com.sbs.exam.interceptor.BeforeActionInterceptor;
+import com.sbs.exam.interceptor.NeedLoginInterceptor;
+import com.sbs.exam.interceptor.NeedLogoutInterceptor;
 import com.sbs.exam.repository.ArticleRepository;
 import com.sbs.exam.repository.MemberRepository;
 import com.sbs.exam.service.ArticleService;
@@ -10,6 +14,9 @@ import com.sbs.exam.service.MemberService;
 import java.sql.Connection;
 
 public class Container {
+  public static BeforeActionInterceptor beforeActionInterceptor;
+  public static NeedLoginInterceptor needLoginInterceptor;
+  public static NeedLogoutInterceptor needLogoutInterceptor;
   public static ArticleRepository articleRepository;
   public static MemberRepository memberRepository;
 
@@ -18,10 +25,15 @@ public class Container {
 
   public static ArticleController articleController;
   public static MemberController memberController;
+  public static HomeController homeController;
 
   public static Connection conn;
 
   public static void init() {
+    beforeActionInterceptor = new BeforeActionInterceptor();
+    needLoginInterceptor = new NeedLoginInterceptor();
+    needLogoutInterceptor = new NeedLogoutInterceptor();
+
     articleRepository = new ArticleRepository();
     memberRepository = new MemberRepository();
 
@@ -30,5 +42,6 @@ public class Container {
 
     articleController = new ArticleController();
     memberController = new MemberController();
+    homeController = new HomeController();
   }
 }
