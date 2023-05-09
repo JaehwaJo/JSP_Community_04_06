e1e7da737e158389bea8233f6889ff6669352d59
jaehwaJo <woghk2233@naver.com>
2023-05-09 19:55:54 +0900

    NeedLoginInterceptor에서 잘못 된 로직 수정

--- src/main/java/com/sbs/exam/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/sbs/exam/interceptor/NeedLoginInterceptor.java
@@ -6,19 +6,15 @@ public class NeedLoginInterceptor extends Interceptor {
   @Override
   public boolean runBeforeAction(Rq rq) {
 
-    if (rq.getControllerTypeName().equals("usr") == false) {
-      return true;
-    }
-
     switch (rq.getActionPath()) {
-      case "/usr/article/write" :
-      case "/usr/article/modify" :
-      case "/usr/article/doModify" :
-      case "/usr/article/delete" :
-      case "/usr/article/doDelete" :
-      case "/usr/article/doLogout" :
-      case "/usr/article/myPage" :
-        if (rq.isNotLogined()) {
+      case "/usr/article/write":
+      case "/usr/article/modify":
+      case "/usr/article/doModify":
+      case "/usr/article/delete":
+      case "/usr/article/doDelete":
+      case "/usr/member/doLogout":
+      case "/usr/member/myPage":
+        if(rq.isNotLogined()) {
           rq.historyBack("로그인 후 이용해주세요.");
           return false;
         }
