25900161a23de27b611744ba706d65cb5acba161
jaehwaJo <woghk2233@naver.com>
2023-05-09 19:55:01 +0900

    회원가입시 null이 발생하던 문제 해결

--- src/main/java/com/sbs/exam/container/Container.java
+++ src/main/java/com/sbs/exam/container/Container.java
@@ -3,6 +3,7 @@ package com.sbs.exam.container;
 import com.sbs.exam.controller.ArticleController;
 import com.sbs.exam.controller.HomeController;
 import com.sbs.exam.controller.MemberController;
+import com.sbs.exam.dto.Member;
 import com.sbs.exam.interceptor.BeforeActionInterceptor;
 import com.sbs.exam.interceptor.NeedLoginInterceptor;
 import com.sbs.exam.interceptor.NeedLogoutInterceptor;
@@ -17,31 +18,33 @@ public class Container {
   public static BeforeActionInterceptor beforeActionInterceptor;
   public static NeedLoginInterceptor needLoginInterceptor;
   public static NeedLogoutInterceptor needLogoutInterceptor;
-  public static ArticleRepository articleRepository;
+
   public static MemberRepository memberRepository;
+  public static ArticleRepository articleRepository;
 
-  public static ArticleService articleService;
   public static MemberService memberService;
+  public static ArticleService articleService;
 
-  public static ArticleController articleController;
+
   public static MemberController memberController;
+  public static ArticleController articleController;
   public static HomeController homeController;
 
   public static Connection conn;
 
   public static void init() {
+    memberRepository = new MemberRepository();
+    articleRepository = new ArticleRepository();
+
+    memberService = new MemberService();
+    articleService = new ArticleService();
+
     beforeActionInterceptor = new BeforeActionInterceptor();
     needLoginInterceptor = new NeedLoginInterceptor();
     needLogoutInterceptor = new NeedLogoutInterceptor();
 
-    articleRepository = new ArticleRepository();
-    memberRepository = new MemberRepository();
-
-    articleService = new ArticleService();
-    memberService = new MemberService();
-
-    articleController = new ArticleController();
     memberController = new MemberController();
+    articleController = new ArticleController();
     homeController = new HomeController();
   }
 }
